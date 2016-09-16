2a26e5984b428ec36a6cf064ad1d92018b302aa9
Balazs Ando <elnesia@gmail.com>
2016-09-16 20:53:20 +0200

    models player move

--- models.py
+++ models.py
@@ -9,7 +9,31 @@ class Sprite:
 class Player(Sprite):
 
     def __init__(self, *args, **kwargs):
+        self.player_id = player_id
         super().__init__(*args, **kwargs)
+
+        def handle_keys(self):
+            """ Handles Keys """
+            key = pygame.key.get_pressed()
+            origin = [self.x, self.y]
+            if [key[pygame.K_DOWN], key[pygame.K_s]][player_id]:
+                if can_move(x=self.x, y=self.y+1):
+                    self.y += 1
+            if [key[pygame.K_UP], key[pygame.K_w]][player_id]:
+                if can_move(x=self.x, y=self.y-1):
+                    self.y -= 1
+            if [key[pygame.K_LEFT], key[pygame.K_a]][player_id]:
+                if can_move(x=self.x-1, y=self.y):
+                    self.x -= 1
+            if [key[pygame.K_RIGHT], key[pygame.K_d]][player_id]:
+                if can_move(x=self.x+1, y=self.y):
+                    self.x += 1
+            return [origin, self.x, self.y]
+
+        @staticmethod
+        def can_move(x, y):
+            return gamefield[x][y] == 0
+
 
 
 class Bomb(Sprite):
